4e4dbd3c0e4bc97d33c8db7c44e2ce772e24f028
Jaime Pastrana García <jaipas01@ucm.es>
2021-06-15 20:02:52 +0200

    Pr 25 update
    Faltaba una plantilla que la profe me ha enviado por las clarifications. Ya da correct.

--- 25_Search_patron/25_Search_patron/25_Search_patron.cpp
+++ 25_Search_patron/25_Search_patron/25_Search_patron.cpp
@@ -2,15 +2,14 @@
 // Usuario del Juez: A56
 
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <list>
 
-typedef long long int lli;
-
 template <class T>
-T MySearch(T const& ini1, T const& fin1, T const& ini2, T const& fin2)
-{
+T Mysearch(T const& ini1, T const& fin1, T const& ini2, T const& fin2) {
+
+
+    // Aqui el código del alumno
     T act = ini1;
     while (act != fin1)//Recorrer la lista inicial
     {
@@ -29,80 +28,57 @@ T MySearch(T const& ini1, T const& fin1, T const& ini2, T const& fin2)
     return fin1;
 }
 
-// función que resuelve el problema
-void resolver(std::list <int>& original, std::list<int> &secuencia) {//Eliminar los fragmentos de la secuencia en la lista original
-    std::list <std::list<int>::iterator> apariciones;
-
-    auto it = MySearch(original.begin(), original.end(), secuencia.begin(), secuencia.end());
-    while (it != original.end())
-    {
-        apariciones.push_back(it);//Guardamos todas las apariciones de la secuencia
-
-        it = MySearch(++it, original.end(), secuencia.begin(), secuencia.end());
+bool resuelveCaso() {
+    int nElem1, nElem2; std::cin >> nElem1 >> nElem2;
+    if (nElem1 == 0 && nElem2 == 0) return false;
+    // Lectura delos datos
+    std::list<int> lista1;
+    for (int i = 0; i < nElem1; ++i) {
+        int aux;
+        std::cin >> aux;
+        lista1.push_back(aux);
     }
-
-    auto ap = apariciones.begin();//Para eliminar secuencias solapadas se tiene una lista de las marcas de todas las secuencias encontradas (solapadas o no)
-    while (ap != apariciones.end())
-    {
-        auto act = ap;
-        ap++;
-
-        int i = 0;
-        while ((ap == apariciones.end() || *act != *ap) && i < secuencia.size())//Si no está solapada... (si está solapada se pasa a la siguiente)
-        {
-            *act = original.erase(*act);
-            i++;
+    std::list<int> lista2;
+    for (int i = 0; i < nElem2; ++i) {
+        int aux;
+        std::cin >> aux;
+        lista2.push_back(aux);
+    }
+    // Elimina todas las apariciones de la lista2 en la lista 1
+    // Utilizando la función Mysearch
+    auto it1 = lista1.begin();
+    while (it1 != lista1.end()) {
+        it1 = Mysearch(it1, lista1.end(), lista2.begin(), lista2.end());
+        if (it1 != lista1.end()) {
+            for (int i = 0; i < lista2.size(); ++i) {
+                it1 = lista1.erase(it1);
+            }
         }
     }
-}
-
-// Resuelve un caso de prueba, leyendo de la entrada la
-// configuración, y escribiendo la respuesta
-bool resuelveCaso() {
-    // leer los datos de la entrada
-    std::list <int> original, secuencia;
-    lli a;
-    int b, aux;
-    std::cin >> a >> b;
-
-    if (a == 0 && b == 0)
-        return false;
-
-    for (lli i = 0; i < a; i++)
-    {
-        std::cin >> aux;
-        original.push_back(aux);
+    // Escribe la lista
+    auto it = lista1.begin();
+    if (it != lista1.end()) {
+        std::cout << *it;
+        ++it;
     }
-    for (int i = 0; i < b; i++)
-    {
-        std::cin >> aux;
-        secuencia.push_back(aux);
-    }
-
-    resolver(original, secuencia);
-
-    // escribir sol
-    for (int elem : original)
-        std::cout << elem << " ";
+    for (; it != lista1.end(); ++it)
+        std::cout << ' ' << *it;
     std::cout << '\n';
+
 
     return true;
 }
 
 int main() {
-    // Para la entrada por fichero.
-    // Comentar para acepta el reto
+
 #ifndef DOMJUDGE
     std::ifstream in("datos.in");
     auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
-#endif 
+#endif
+
+        while (resuelveCaso()) {} //Resolvemos todos los casos
 
 
-    while (resuelveCaso())
-        ;
-
-
-    // Para restablecer entrada. Comentar para acepta el reto
 #ifndef DOMJUDGE // para dejar todo como estaba al principio
     std::cin.rdbuf(cinbuf);
     system("PAUSE");
