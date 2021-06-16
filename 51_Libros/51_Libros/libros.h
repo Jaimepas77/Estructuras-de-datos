859addfc029cd5e6eefa30d9e9737e8a97571290
Jaime Pastrana García <jaipas01@ucm.es>
2021-06-16 19:15:33 +0200

    Pr 51 successful
    Usar un map ordenado por nº de ventas para lograr eficiencia... xd

--- 51_Libros/51_Libros/libros.h
+++ 51_Libros/51_Libros/libros.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <map>
 #include <list>
 #include <utility>
 #include <stdexcept>
@@ -14,18 +15,18 @@ private:
 
 	struct bookInfo {
 		int unidades = 0;
-		std::list<std::pair<libro, int>>::iterator it;//Iterador a la lista topVentas
+		int vendidos = 0;
+		std::list<libro>::iterator it;//Iterador a la lista topVentas
 	};
 
 	std::unordered_map<libro, bookInfo> catalog;//Catalogo de libros y sus unidades disponibles
-	std::list<std::pair<libro, int>> topVentas;	//lista de compras ordenada (par libro-ventas) //ERROR: SÓLO HACE FALTA MANTENER LOS 10 MEJORES!!!
+	//std::list<std::pair<libro, int>> topVentas;	//lista de compras ordenada (par libro-ventas)
+	std::map<int, std::list<libro>> topVentas;
 
 public:
 	void nuevoLibro(int n, libro x) 
 	{
 		catalog[x].unidades += n;//Añadir unidades
-
-		catalog[x].it = topVentas.end();
 	}
 
 	void comprar(libro x)
@@ -38,9 +39,17 @@ public:
 		else
 		{
 			it->second.unidades--;
-
+			it->second.vendidos++;
+			
 			//Registrar compra para la lista del top 10
-			if (it->second.it == topVentas.end())
+			if (it->second.vendidos > 1)//Si antes ya estaba en la lista de vendidos...
+			{
+				topVentas[it->second.vendidos - 1].erase(it->second.it);//Borrar
+			}
+			topVentas[it->second.vendidos].push_front(x);//Añadir
+			it->second.it = topVentas[it->second.vendidos].begin();//Guardar puntero
+			
+			/*if (it->second.it == topVentas.end())
 			{
 				topVentas.push_back({ x, 1 });//Añadir a la lista de ventas (top 10)
 				it->second.it = --topVentas.end();
@@ -58,7 +67,7 @@ public:
 			
 			ref = topVentas.insert(ref, *it->second.it);
 			topVentas.erase(it->second.it);
-			it->second.it = ref;
+			it->second.it = ref;*/
 		}
 	}
 
@@ -74,11 +83,8 @@ public:
 		if (it != catalog.end())
 		{
 			//Eliminar del top 10...
-			if (it->second.it != topVentas.end())
-			{
-				topVentas.erase(it->second.it);
-				it->second.it = topVentas.end();
-			}
+			if (it->second.vendidos != 0)
+				topVentas[it->second.vendidos].erase(it->second.it);
 
 			catalog.erase(it);
 		}
@@ -97,12 +103,40 @@ public:
 	{
 		//Recorrer los diez primeros libros del top y meterlos en un vector
 		std::vector<libro> ret;
+		if (topVentas.empty())
+			return ret;
+
 		int i = 0;
-		for (auto elem : topVentas)
+		auto it = topVentas.end();
+		it--;
+		while (it != topVentas.begin())
 		{
-			if (i >= 10 || elem.second == 0) break;
-			ret.push_back(elem.first);
+			auto libs = it->second.begin();
+
+			while (libs != it->second.end())
+			{
+				ret.push_back(*libs);
+				i++;
+
+				if (i >= 10)
+					return ret;
+
+				libs++;
+			}
+			it--;
+		}
+
+		auto libs = it->second.begin();
+
+		while (libs != it->second.end())
+		{
+			ret.push_back(*libs);
 			i++;
+
+			if (i >= 10)
+				return ret;
+
+			libs++;
 		}
 		return ret;
 	}
